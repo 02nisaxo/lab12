26244d9ea7f772850bbbbe841a5ead96042e0150
02nisaxo <haytanisanur@gmail.com>
2024-11-24 19:52:52 -0500

    Add files via upload

---
+++ lab12.sql
@@ -0,0 +1,41 @@
+SELECT course_id, course_name - semester as course_and_semester
+FROM courses
+
+SELECT course_id, course_name, lab_time
+FROM courses
+WHERE lab_day = 'Friday';
+
+SELECT course_id, title, due_date
+FROM assignments
+WHERE due_date>DATE
+
+
+SELECT status, COUNT(*) AS assignment_count
+FROM assignments
+GROUP BY status;
+
+
+SELECT course_name
+FROM courses
+ORDER BY LENGTH(course_name) DESC
+LIMIT 1;
+
+
+SELECT course_name
+FROM courses
+WHERE LENGTH(course_name) = (
+  SELECT MAX(LENGTH(course_name))
+  FROM courses
+);
+
+SELECT UPPER(course_name) AS uppercase_name
+FROM courses; 
+
+SELECT title
+FROM assignments
+WHERE due_date LIKE '%-09-%';
+
+
+SELECT *
+FROM assignments
+WHERE due_date IS NULL;
